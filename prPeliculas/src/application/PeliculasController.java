6d176c857703c10bfba6e663aff0f96eedf47e1f
JenaroLeal <jenaroleal@hotmail.com>
2022-12-06 12:31:07 +0100

    actualizado

--- prPeliculas/src/application/PeliculasController.java
+++ prPeliculas/src/application/PeliculasController.java
@@ -1,5 +1,10 @@
 package application;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -14,7 +19,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 public class PeliculasController {
-	
+
 	@FXML
 	private TextField txtNombre;
 	@FXML
@@ -37,11 +42,13 @@ public class PeliculasController {
 	private Button btnAñadir;
 	@FXML
 	private Button btnBorrar;
-	
-	private ObservableList<Pelicula> listaPeliculas = FXCollections.observableArrayList(new Pelicula("Tarzán",120,"Aventura","+3"));
-	public ObservableList<String> generos = FXCollections.observableArrayList("Accion","Aventura","Drama","Comedia","Sci-Fi");
-	public ObservableList<String> pegis = FXCollections.observableArrayList("+3","+7","+12","+18");
-	
+
+	private ObservableList<Pelicula> listaPeliculas = FXCollections
+			.observableArrayList(new Pelicula("Tarzán", 120, "Aventura", "+3"));
+	public ObservableList<String> generos = FXCollections.observableArrayList("Accion", "Aventura", "Drama", "Comedia",
+			"Sci-Fi");
+	public ObservableList<String> pegis = FXCollections.observableArrayList("+3", "+7", "+12", "+18");
+
 	@FXML
 	private void initialize() {
 		opcionGenero.setItems(generos);
@@ -51,32 +58,79 @@ public class PeliculasController {
 		columGenero.setCellValueFactory(new PropertyValueFactory<>("genero"));
 		columPegi.setCellValueFactory(new PropertyValueFactory<>("pegi"));
 		
-		tablePeliculas.setItems(listaPeliculas);
+		ObservableList listaPeliculasBD=getPeliculasBD();
 		
+		tablePeliculas.setItems(listaPeliculas);
+		tablePeliculas.setItems(listaPeliculasBD);
 	}
-	
+
+	private ObservableList<Pelicula> getPeliculasBD() {
+
+		ObservableList<Pelicula> listaPeliculasBD = FXCollections.observableArrayList();
+
+		DatabaseConnection dbConnection = new DatabaseConnection();
+		Connection connection = dbConnection.getConnection();
+		String query = "select * from peliculas";
+		try {
+			PreparedStatement ps = connection.prepareStatement(query);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				Pelicula p = new Pelicula(rs.getString("nombre"), rs.getInt("duracion"), rs.getString("genero"),
+						rs.getString("pegi"));
+				listaPeliculasBD.add(p);
+			}
+			connection.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return listaPeliculasBD;
+
+	}
+
 	@FXML
 	public void añadirPelicula(ActionEvent event) {
-		
-		if(txtNombre.getText().isEmpty() || txtDuracion.getText().isEmpty()
-				|| opcionGenero.getSelectionModel().isEmpty() || opcionPegi.getSelectionModel().isEmpty()){
-			
+
+		if (txtNombre.getText().isEmpty() || txtDuracion.getText().isEmpty()
+				|| opcionGenero.getSelectionModel().isEmpty() || opcionPegi.getSelectionModel().isEmpty()) {
+
 			Alert alerta = new Alert(AlertType.ERROR);
 			alerta.setTitle("Error");
 			alerta.setHeaderText("Faltan datos por insertar");
 			alerta.setContentText("Completa todos los campos");
 			alerta.showAndWait();
-		}else {
-			if(esNumero(txtDuracion.getText())) {
-				Pelicula p = new Pelicula(txtNombre.getText(),Integer.parseInt(txtDuracion.getText()),
+		} else {
+			if (esNumero(txtDuracion.getText())) {
+				Pelicula p = new Pelicula(txtNombre.getText(), Integer.parseInt(txtDuracion.getText()),
 						opcionGenero.getValue().toString(), opcionPegi.getValue().toString());
 				listaPeliculas.add(p);
-				
+
 				txtNombre.clear();
 				txtDuracion.clear();
 				opcionGenero.getSelectionModel().clearSelection();
 				opcionPegi.getSelectionModel().clearSelection();
-			}else {
+
+				DatabaseConnection dbConnection = new DatabaseConnection();
+				Connection connection = dbConnection.getConnection();
+				String query = "insert into peliculas (nombre, duracion, genero, pegi)" + "values (?,?,?,?)";
+
+				try {
+					PreparedStatement ps = connection.prepareStatement(query);
+					ps.setString(1, p.getNombre());
+					ps.setInt(2, p.getDuracion());
+					ps.setString(3, p.getGenero());
+					ps.setString(4, p.getPegi());
+					ps.executeUpdate();
+					connection.close();
+					ObservableList listaPeliculasBd = getPeliculasBD();
+					tablePeliculas.setItems(listaPeliculasBd);
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			} else {
 				Alert alerta = new Alert(AlertType.ERROR);
 				alerta.setTitle("Error al insertar");
 				alerta.setHeaderText("No se ha introducido un número en el precio");
@@ -85,7 +139,7 @@ public class PeliculasController {
 			}
 		}
 	}
-	
+
 	public boolean esNumero(String s) {
 		try {
 			Integer.parseInt(s);
@@ -94,33 +148,18 @@ public class PeliculasController {
 			return false;
 		}
 	}
-	
+
 	public void borrarPelicula(ActionEvent event) {
 		int indice = tablePeliculas.getSelectionModel().getSelectedIndex();
-		if(indice<0) {
+		if (indice < 0) {
 			Alert alerta = new Alert(AlertType.ERROR);
 			alerta.setTitle("Error al borrar");
 			alerta.setHeaderText("No se puede borrar");
 			alerta.setContentText("Por favor elige una fila");
 			alerta.showAndWait();
-		}else {
+		} else {
 			tablePeliculas.getItems().remove(indice);
 		}
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
